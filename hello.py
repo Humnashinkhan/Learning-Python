d6fc7cbb7fe2b43814d4500834d6d6314c21e87b
Humnashin Khan <humnashin9955@gmail.com>
2026-03-21 21:07:31 +0530

    data types

--- hello.py
+++ hello.py
@@ -2,3 +2,36 @@ print("Hello, Python!", 7)
 print(5)
 print("bye")
 print(12*12)
+
+
+a = 347863845834
+print(a)
+b = "Humnashin"
+print(b)    
+
+data type is what kind of data a variable is storing
+types of data types
+
+1. numeric type
+ int - whole number
+ float - decimal number
+ complex - for complex number 
+
+2. Sequence Type
+  Ordered collection of items.
+str → text
+list → changeable collection - Mutable
+tuple → fixed collection - immutable
+
+3. Set Type 🎯
+
+Unordered collection with no duplicates.
+ Dictionary - store values in key value pairs 
+
+ Boolean - Represent true and false 
+
+None Type 🚫
+
+Represents no value.
+
+x = none
