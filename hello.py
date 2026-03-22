df6663dd333375d367300cbfe04d7fc807192c7a
Humnashin Khan <humnashin9955@gmail.com>
2026-03-22 16:27:08 +0530

    calculator

--- hello.py
+++ hello.py
@@ -9,29 +9,39 @@ print(a)
 b = "Humnashin"
 print(b)    
 
-data type is what kind of data a variable is storing
-types of data types
+# data type is what kind of data a variable is storing
+# types of data types
 
-1. numeric type
- int - whole number
- float - decimal number
- complex - for complex number 
+# 1. numeric type
+#  int - whole number
+#  float - decimal number
+#  complex - for complex number 
 
-2. Sequence Type
-  Ordered collection of items.
-str → text
-list → changeable collection - Mutable
-tuple → fixed collection - immutable
+# 2. Sequence Type
+#   Ordered collection of items.
+# str → text
+# list → changeable collection - Mutable
+# tuple → fixed collection - immutable
 
-3. Set Type 🎯
+# 3. Set Type 🎯
 
-Unordered collection with no duplicates.
- Dictionary - store values in key value pairs 
+# Unordered collection with no duplicates.
+#  Dictionary - store values in key value pairs 
 
- Boolean - Represent true and false 
+#  Boolean - Represent true and false 
 
-None Type 🚫
+# None Type 🚫
 
-Represents no value.
+# Represents no value.
 
-x = none
+# x = none
+
+# Create a Calculator 
+
+a = 30
+b = 5
+
+print("The value of", a, "+", b, "is:", a + b)
+print("The value of", a, "-", b, "is:", a - b)
+print("The value of", a, "*", b, "is:", a * b)
+print("The value of", a, "/", b, "is:", a / b)
